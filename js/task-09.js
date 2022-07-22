139fe46ec8a46185a3d3231fa7a9de418e52794d
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-07-22 11:41:37 +0200

    Added new tasks

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const changeColorBtnRef = document.querySelector('button.change-color');
+const colorLabelRef = document.querySelector('span.color');
+
+const changeColorBtnClickHandler = event => {
+  const newColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = newColor;
+  colorLabelRef.textContent = newColor;
+};
+
+changeColorBtnRef.addEventListener('click', changeColorBtnClickHandler);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,59 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const inputControlRef = document.querySelector('#controls>input');
+const createBtnRef = document.querySelector('[data-create]');
+const destroyBtnRef = document.querySelector('[data-destroy]');
+const boxesBlockRef = document.getElementById('boxes');
+
+const getNextBoxItemSizes = () => {
+  const lastBoxItemRef = boxesBlockRef.lastElementChild;
+
+  if (lastBoxItemRef === null) return { width: 30, height: 30 };
+
+  console.log(lastBoxItemRef.style.width);
+  return {
+    width: Number.parseInt(lastBoxItemRef.style.width) + 10,
+    height: Number.parseInt(lastBoxItemRef.style.height) + 10,
+  };
+};
+
+const createBoxes = amount => {
+  const boxItems = [];
+  const nextBoxItemSizes = getNextBoxItemSizes();
+
+  for (let i = 0; i < amount; i += 1) {
+    const newBoxItemRef = document.createElement('div');
+    newBoxItemRef.style.width = `${nextBoxItemSizes.width}px`;
+    newBoxItemRef.style.height = `${nextBoxItemSizes.height}px`;
+    const newBoxColor = getRandomHexColor();
+    console.log(newBoxColor);
+    newBoxItemRef.style.backgroundColor = newBoxColor;
+
+    boxItems.push(newBoxItemRef);
+
+    nextBoxItemSizes.width += 10;
+    nextBoxItemSizes.height += 10;
+  }
+  boxesBlockRef.append(...boxItems);
+};
+
+const createBtnClickHandler = () => {
+  const amount = inputControlRef.value;
+
+  if (amount <= 0) {
+    alert('Invalid value entered.');
+    return;
+  }
+
+  createBoxes(amount);
+};
+
+createBtnRef.addEventListener('click', createBtnClickHandler);
+
+destroyBtnRef.addEventListener('click', () => {
+  boxesBlockRef.innerHTML = '';
+});
